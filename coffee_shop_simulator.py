dbc580dc84c2920230ae6a7770b5a54657614082
Shelby Kelley <shelbya.kelley@gmail.com>
2025-11-23 14:26:06 -0700

    chapter11: simplified temp distribution with numpy and made sales less predictable

--- coffee_shop_simulator.py
+++ coffee_shop_simulator.py
@@ -1,11 +1,16 @@
 import random
 import re
+import numpy
 from utilities import *
 
 class CoffeeShopSimulator:
     # Minimum and maximum temperatures
     TEMP_MIN = 20
     TEMP_MAX = 90
+
+    # Length of temperature list
+    # (Higher produces more realistic curve)
+    SERIES_DENSITY = 300
 
     def __init__(self, player_name, shop_name):
         # Set player and show names
@@ -43,7 +48,8 @@ class CoffeeShopSimulator:
                 cup_price = convert_to_float(response)
             
             # Do they want to buy more coffee inventory
-            response = prompt("Want to buy more coffee? (hit ENTER for none or enter number)", False)
+            print("\nIt cost $1 for the necessary inventory to make a cup of coffee.")
+            response = prompt("Want to buy more so you can make more coffee? (hit ENTER for none or enter number)", False)
             if response:
                 if not self.buy_coffee(response):
                     print("Could not buy additional coffee.")
@@ -75,7 +81,7 @@ class CoffeeShopSimulator:
 
     def simulate(self, temperature, advertising, cup_price):
         # Find out how many cups were sold
-        cups_sold = self.daily_sales(temperature, advertising)
+        cups_sold = self.daily_sales(temperature, advertising, cup_price)
         # Save the sames data for today
         self.sales.append({
             "day": self.day,
@@ -103,26 +109,13 @@ class CoffeeShopSimulator:
             return False
 
     def make_temp_distribution(self):
-        # Will make this more mathematically advanced later
-        temps = []
-        # First, find the average between TEMP_MIN and TEMP_MAX
-        avg_temp = (self.TEMP_MIN + self.TEMP_MAX) / 2
-        # Find the distance between TEMP_MAX and the average
-        max_dist_from_avg = self.TEMP_MAX - avg_temp
-        # Loop through all possible temperatures
-        for i in range(self.TEMP_MIN, self.TEMP_MAX):
-            # How far away is the temperature from the average?
-            # abs() gives us the absolute value
-            dist_from_avg = abs(avg_temp - i)
-            # How far away is the dist_from_avg from the maximum
-            dist_from_max_dist = max_dist_from_avg - dist_from_avg
-            # If the value is zero, make it 1
-            if dist_from_max_dist == 0:
-                dist_from_max_dist = 1
-            # Append the output of x_of_y to temps
-            for t in x_of_y(int(dist_from_max_dist), i):
-                temps.append(t)
-        return temps
+        # Create series of numbers between TEMP_MIN and TEMP_MAX
+        series = numpy.linspace(self.TEMP_MIN, self.TEMP_MAX, self.SERIES_DENSITY)
+        # Obtain mean and stardard deviation from the series
+        mean = numpy.mean(series)
+        std_dev = numpy.std(series)
+        # Calculate probability density and return the list it creates
+        return (numpy.pi * std_dev) * numpy.exp(-0.5 * ((series - mean) / std_dev) ** 2)
 
     def increment_day(self):
         self.day += 1
@@ -134,8 +127,19 @@ class CoffeeShopSimulator:
     def day_header(self):
         print(f"\n-------| Day {self.day} @ {self.shop_name} |-------")
 
-    def daily_sales(self, temperature, advertising):
-        sales = int((self.TEMP_MAX - temperature) * (advertising * 0.5))
+    def daily_sales(self, temperature, advertising, cup_price):
+        # Randomize advertising effectiveness
+        adv_coefficient = random.randint(20, 80) / 100
+        # Higher priced cofee doesn't sell as well
+        price_coefficient = int((cup_price * (random.randint(50, 250) / 100)))
+        # Run the sales figures!
+        sales = int((self.TEMP_MAX - temperature) * (advertising * adv_coefficient))
+        # If price is too high, we don't sell anything
+        if price_coefficient > sales:
+            sales = 0
+        else:
+            sales -= price_coefficient
+
         if sales > self.coffee_inventory:
             sales = self.coffee_inventory
             print("You would have sold more coffee but you ran out. Be sure to buy more inventory.")
@@ -145,4 +149,4 @@ class CoffeeShopSimulator:
     def weather(self):
         # Generate a random temperature between TEMP_MIN and TEMP_MAX
         # We'll consider seasons later on, but this is good enough for now
-        return random.choice(self.temps)
+        return int(random.choice(self.temps))
